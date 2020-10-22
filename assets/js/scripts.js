8090d0ba22229d87576ce13a5d99d83c44bf3605
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-22 10:06:27 +0000

    Add feat: sprite automatic movement
    Also fixed the playerHeight and playerWidth: this was not being recognised because it was a string instead of a number.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,8 +7,8 @@ const images = {};
 images.player = new Image();
 images.player.src = "../assets/img/cuphead.png";
 
-const playerWidth = "103.0625";
-const playerHeight = "113.125";
+const playerWidth = 103.0625;
+const playerHeight = 113.125;
 let playerFrameX = 3;
 let playerFrameY = 3;
 let playerX = 0;
@@ -23,6 +23,22 @@ function animate() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight,
     playerX, playerY, playerWidth, playerHeight);
+    // Allows for the sprite's walking animation
+    if (playerFrameX < 13) {
+        playerFrameX ++;
+    }
+    else {
+        playerFrameX = 3;
+    }
+    // Moves the sprite across the canvas
+    if (playerX < canvas.width + playerWidth) {
+        playerX += playerSpeed;
+    }
+    // when the sprite walks past the right edge of canvas, we will reset it and hide it just before the left edge of canvas
+    // like an infinite loop
+    else {
+        playerX = 0 - playerWidth;
+    }
 }
 
 window.onload = setInterval(animate, 1000/30);      // RequestAnimationFrame will be too fast
